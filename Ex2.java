0672d729fd780b97029cab741fe6b01690227e5c
Hari8379 <nhari8793@gmail.com>
2026-04-16 15:37:39 +0530

    first commit

---
+++ Ex2.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Arithmetic Operators
+
+        // int a=10;
+        // int b=20;
+        // int c=a+b;
+        // int d=a-b;
+        // int e=a*b;
+        // int f=b/a;
+        // int g=a%b;
+        // System.out.println("The answer is: " + c);
+        // System.out.println(d);
+        // System.out.println(e+" "+f);
+        // System.out.println(g);
+
+        //Relational Operators
+
+        // int a=10;
+        // int b=20;
+        // System.out.println(a<b);
+        // System.out.println(a>b);
+        // System.out.println(a<=b);   
+        // System.out.println(a>=b);
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+
+        // Logical Operators
+
+        // int a=10;
+        // int b=20;
+        // System.out.println(a<b && a>5);
+        // System.out.println(a>b || a>5);
+        // System.out.println(!(a<b));
+
+        // Assignment Operators
+
+        // int a=10;
+        // int b=a+=5;
+        // int c=a-=5;
+        // int d=a*=5;
+        // int e=a/=5;
+        // int f=a%=5;
+        
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
+        // System.out.println(f);
+
+
+        //Age Calculator
+        // System.out.print("Enter your birth year: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter current year: ");
+        // int b = sc.nextInt();
+        // int age=b-a;
+        // System.out.println("Your age is: " + age);
+
+        //Odd or Even
+        
+        // int num;
+        // System.out.print("Enter a number: ");
+        // num = sc.nextInt();
+        // if(num % 2 == 0) {
+        //     System.out.println(num + " is an even number.");
+        // } 
+        // else {
+        //     System.out.println(num + " is an odd number.");
+        // }
+
+
+        //
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,56 @@
+public class Test{
+    public static void main(String[] args) {
+        
+        // First Code
+        
+        // int age = 21;
+        // double salary = 50000.0;
+        // char grade = 'A';
+
+        // System.out.println(age);
+        // System.out.println(salary);
+        // System.out.println(grade);
+
+        //Second Code
+        
+        // String name = "Arun";
+        // int age = 21;
+        // String course = "B.E";
+        // double percentage = 85.5; 
+
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("Course: " + course);
+        // System.out.println("Percentage: " + percentage);
+
+        // Third Code
+
+        // int a=10;
+        // int b=5;
+    
+        // System.out.println("Addition: " + (a + b));
+        // System.out.println("Subtraction: " + (a - b));
+        // System.out.println("Multiplication: " + (a * b));
+        // System.out.println("Division: " + (a / b));
+
+        // Fourth Code
+
+        // System.out.println("Name : Ravi");
+        // System.out.println("City : Mysore");
+
+        // Fifth Code
+
+        // int a=5,b=10,temp;
+
+        // temp=a;
+        // a=b;
+        // b=temp;
+        // System.out.println(a + " " + b);
+
+        // Sixth Code
+
+        // double r = 5;
+        // double area = 3.14 * r * r;
+        // System.out.println("Area of the circle: " + area);
+    }
+}
